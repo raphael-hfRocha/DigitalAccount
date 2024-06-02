26b5de5d2f10cf98b807b27bb55a333a3b76c22e
Raphael Henrique Fernandes Rocha <rhfrocha@prodam.sp.gov.br>
2024-06-01 21:34:04 -0300

    Project creation

---
+++ InfraRepository/Repositories/AddCustomer/AddCustomerRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Domain.Contracts.Repositories.AddCustomer;
+using Domain.Entities;
+
+namespace InfraRepository.Repositories.AddCustomer;
+public class AddCustomerRepository : IAddCustomerRepository
+{
+    private readonly IList<Customer> _customers = new List<Customer>();
+
+
+    public void AddCustomer(Customer customer)
+    {
+        _customers.Add(customer);
+        throw new NotImplementedException();
+    }
+}
---
+++ WebApi/Models/AddCustomer/AddCustomerInput.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApi.Models.AddCustomer;
+
+public class AddCustomerInput
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Document { get; set; }
+
+    public AddCustomerInput(string name, string email, string document)
+    {
+        Name = name;
+        Email = email;
+        Document = document;
+    }
+}
